20613d1d27ffcaeda763bc357efe0b77c816f501
anhquanhus0902 <pvaquans22@gmail.com>
2022-07-14 09:32:08 +0700

    change slide

--- js/scripts.js
+++ js/scripts.js
@@ -2,22 +2,22 @@ $(document).ready(function() {
     showDivs(1);
 });
 
-var sliceDivClassName = "month";
-var sliceIndex = 1;
+var slideDivClassName = "month";
+var slideIndex = 1;
 
 function showDivs(n) {
-    var numOfMonthDivs = document.getElementsByClassName(sliceDivClassName).length+1;
-    var cssSelector = '.' + sliceDivClassName;
+    var numOfMonthDivs = document.getElementsByClassName(slideDivClassName).length+1;
+    var cssSelector = '.' + slideDivClassName;
     var numOfDisplayMonthDivs = 3;
-    if (n >= 1 && sliceIndex + numOfDisplayMonthDivs > numOfMonthDivs) {
+    if (n >= 1 && slideIndex + numOfDisplayMonthDivs > numOfMonthDivs) {
         return;
     }
-    if (n <= -1 && sliceIndex == 2) {
+    if (n <= -1 && slideIndex == 2) {
         return;
     }
-    sliceIndex += n;
+    slideIndex += n;
     $(cssSelector).hide();
-    for (var i = sliceIndex; i < sliceIndex+numOfDisplayMonthDivs; ++i) {
-        $(cssSelector + ':nth-child(' + i + ')').show();
+    for (var i = slideIndex; i < slideIndex+numOfDisplayMonthDivs; ++i) {
+        $(cssSelector + ':nth-child(' + i + ')').show({direction: "right"});
     }
 }
